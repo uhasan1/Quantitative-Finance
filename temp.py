d08da4b9c4ae3b2286d8f981230a0e8f2102099e
clementyeehw <36322726+clementyeehw@users.noreply.github.com>
2018-07-03 00:55:03 +0800

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,31 +1,34 @@
-# Import Python Libraries 
-import survey
+# Gaussian Copula
 
-# Import dataset
-table = survey.Pregnancies()
-table.ReadRecords()
-print ("Number of pregnancies is", len(table.records))
+import numpy as np
+import difflib as dl
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Calculate the descriptive statistics of babies that are live births
-liveBirths = 0
-firstBirths = 0
-notFirstBirths = 0
-firstDuration = 0
-notFirstDuration = 0
-for babies in table.records:
-    if babies.outcome == 1:
-        liveBirths += 1
-        if babies.birthord == 1:
-            firstBirths += 1
-            firstDuration = firstDuration + babies.prglength
-        else:
-            notFirstBirths += 1
-            notFirstDuration = notFirstDuration + babies.prglength
-            
-print ("Number of livebirth babies is", liveBirths)
-print ("Number of livebirth babies that are first born is", firstBirths)
-print ("Number of livebirth babies that are not first born is", notFirstBirths)
-print ("Average pregnancy length of livebirth babies that are first born is", firstDuration/firstBirths)
-print ("Average pregnancy length of livebirth babies that are not first born is", notFirstDuration/notFirstBirths)
-print ("Difference is", firstDuration/firstBirths - notFirstDuration/notFirstBirths, "weeks or", 
-       24 * 7 * (firstDuration/firstBirths - notFirstDuration/notFirstBirths), "hours")
+sample_list = []
+actual_list = []
+df = pd.read_excel(r'C:\Users\r15\.spyder-py3\testdifflib.xlsx')
+# Remember to include .dropna for both Dataframes #
+sample_list = np.array([df['Sample']])
+sample_list.resize(6,1)
+actual_list = np.array(df['Actual'].dropna())
+
+def name_match(sample, actual):
+    print(sample)
+#    s = sample.flatten()
+    
+#    for company in s:
+#        print(company)
+#        name = dl.get_close_matches(company, actual, 1, 0.8)
+#        if len(name) != 0:
+#            result = np.array(name)
+
+myfunc = np.vectorize(name_match)
+myfunc(sample_list, actual_list)
+
+#https://jakevdp.github.io/PythonDataScienceHandbook/02.05-computation-on-arrays-broadcasting.html
+#N=30
+#x=np.random.normal(0,1,N)
+#X=np.vstack((x,x))
+#cov=np.cov(X)
+#print (cov)
